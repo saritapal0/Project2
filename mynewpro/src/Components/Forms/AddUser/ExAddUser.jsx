6845cdf4555bdde5e5ecb7ff9e3d72de186659d1
saritapal0 <159772730+saritapal0@users.noreply.github.com>
2024-02-19 12:40:27 +0530

    Project2

---
+++ mynewpro/src/Components/Forms/AddUser/ExAddUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ExAddUser() {
+  return (
+    <div>
+      <h1>fnkjekjfiw</h1>
+    </div>
+  )
+}
--- mynewpro/src/Components/Forms/FormLayouts/FormLayouts.jsx
+++ mynewpro/src/Components/Forms/FormLayouts/FormLayouts.jsx
@@ -1,88 +1,258 @@
-import React, { useState } from "react";
+import React from "react";
+import {
+    Card,
+    CardContent,
+    Divider,
+    Box,
+    Typography,
+    TextField,
+    FormControl,
+    Button,
+    Grid,
+    MenuItem,
+    InputLabel,
+    Select,
+} from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
 
-
- import "../../../Components/Forms/FormLayouts/style.css";
-
-function FormLayouts() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // User Login info
-  const database = [
+const numbers = [
     {
-      username: "user1",
-      password: "pass1"
+        value: "one",
+        label: "One",
     },
     {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
+        value: "two",
+        label: "Two",
+    },
+    {
+        value: "three",
+        label: "Three",
+    },
+    {
+        value: "four",
+        label: "Four",
+    },
+    {
+        value: "five",
+        label: "Five",
+    },
+];
 
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
+const AddUser = () => {
 
-    var { uname, pass } = document.forms[0];
+    const [selectedFile, setSelectedFile] = React.useState(null);
 
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
+    const handleFileSelect = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
 
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
+    const [state, setState] = React.useState({
+        gender: "",
+    });
+
+    const handleChange = (event) => {
+        setState({ ...state, gender: event.target.value });
+    };
+
+    const [number, setNumber] = React.useState("");
+
+    const handleChange3 = (event) => {
+        setNumber(event.target.value);
+    };
+
+    return (
+        <div>
+            {/* ------------------------------------------------------------------------------------------------ */}
+            {/* Basic Checkbox */}
+            {/* ------------------------------------------------------------------------------------------------ */}
+            <Card
+                variant="outlined"
+                sx={{
+                    p: 0,
+                }}
+            >
+                <Box
+                    sx={{
+                        padding: "15px 30px",
+                    }}
+                    display="flex"
+                    alignItems="center"
+                >
+                    <Box flexGrow={1}>
+                        <Typography
+                            sx={{
+                                fontSize: "18px",
+                                fontWeight: "500",
+                            }}
+                        >
+                            Add yourself
+                        </Typography>
+                    </Box>
+                </Box>
+                <Divider />
+                <CardContent
+                    sx={{
+                        padding: "30px",
+                    }}
+                >
+                    <form>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    id="first-name"
+                                    label="First Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    sx={{
+                                        mb: 2,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    id="last-name"
+                                    label="Last Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    sx={{
+                                        mb: 2,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
+                                    <InputLabel id="gender-label">Gender</InputLabel>
+                                    <Select
+                                        labelId="gender-label"
+                                        id="gender"
+                                        value={state.gender}
+                                        onChange={handleChange}
+                                        label="Gender"
+                                    >
+                                        <MenuItem value="male">Male</MenuItem>
+                                        <MenuItem value="female">Female</MenuItem>
+                                        <MenuItem value="other">Other</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    id="dob"
+                                    label="Date of Birth"
+                                    type="date"
+                                    variant="outlined"
+                                    fullWidth
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    sx={{
+                                        mb: 2,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    id="phone-number"
+                                    label="Phone Number"
+                                    variant="outlined"
+                                    fullWidth
+                                    sx={{
+                                        mb: 2,
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    id="email"
+                                    label="Email Address"
+                                    variant="outlined"
+                                    fullWidth
+                                    sx={{
+                                        mb: 2,
+                                    }}
+                                />
+                            </Grid>
+
+
+                            <Grid item xs={12} md={6}>
+                                <FormControl fullWidth variant="outlined" sx={{ mb: 2 }}>
+                                    <InputLabel id="Higher-Qualification">Higher Qualification</InputLabel>
+                                    <Select
+                                        labelId="Higher-Qualification"
+                                        id="Higher-Qualification"
+                                        value={number}
+                                        onChange={handleChange3}
+                                        label="Higher-Qualification"
+                                    >
+                                        {numbers.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                                {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12} md={6}>
+                                <Button
+                                    component="label"
+                                    role={undefined}
+                                    variant="contained"
+                                    tabIndex={-1}
+                                    startIcon={<CloudUploadIcon />}
+                                >
+                                    Upload Resume
+                                    <VisuallyHiddenInput
+                                        type="file"
+                                        onChange={handleFileSelect} />
+                                </Button>
+                                {selectedFile && (
+                                    <Typography sx={{ mt: 1 }}>
+                                        Selected File: {selectedFile.name}
+                                    </Typography>
+                                )}
+                            </Grid>
+
+                            <Grid item xs={12} md={12}>
+                                <TextField
+                                    id="Intro"
+                                    label="Describe Yourself..."
+                                    fullWidth
+                                    multiline
+                                    minRows={8}
+                                    maxRows={6}
+                                    sx={{
+                                        mb: 1,
+                                    }}
+                                />
+                            </Grid>
+                        </Grid>
+                        <div>
+                            <br />
+                            <Button color="primary" variant="contained">
+                                Submit
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
     );
+};
 
-  // JSX code for login form
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
-
-  return (
-    <div className="app">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
-    </div>
-  );
-}
-
-export default FormLayouts;
+export default AddUser;
